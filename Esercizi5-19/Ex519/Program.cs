e493e2d384bec7ff3c6035e99adf2200ab97597c
Besky037 <Leo.Betti05@gmail.com>
2025-05-19 11:49:20 +0200

    Add files via upload

---
+++ Esercizi5-19/Ex519/Program.cs
@@ -0,0 +1,45 @@
+﻿class Esercizi
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Inserire i fattori:");
+        int fattore1 = int.Parse(Console.ReadLine());
+        int fattore2 = int.Parse(Console.ReadLine());
+
+        int somma = Somma(fattore1, fattore2);
+        int moltiplica = Moltiplica(fattore1, fattore2);
+        int dividi = Dividi(fattore1, fattore2);
+
+        StampaRisultato("Somma", somma);
+        StampaRisultato("moltiplicazione", moltiplica);
+        StampaRisultato("divisione", dividi);
+    }
+
+    static int Somma(int fattore1, int fattore2)
+    {
+        return fattore1 + fattore2;
+    }
+
+    static int Moltiplica(int fattore1, int fattore2)
+    {
+        return fattore1 * fattore2;
+    }
+
+    static int Dividi(int fattore1, int fattore2)
+    {
+        if (fattore2 == 0)
+        {
+            Console.WriteLine("We non posso dividere pe' 0");
+            return 0;
+        }
+        else
+        {
+            return fattore1 / fattore2;
+        }
+    }
+
+    static void StampaRisultato(string operazione, int risultato)
+    {
+        Console.WriteLine($"Il risultato della {operazione} è {risultato}");
+    }
+}

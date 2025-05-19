f328ec5305ba69c34ca616ffd7ad2a7f303daf50
Besky037 <Leo.Betti05@gmail.com>
2025-05-19 16:40:53 +0200

    Add files via upload

---
+++ Esercizi5-19-2/ClassiEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Studente
+{
+    public string Nome;
+    public int Matricola;
+    public double MediaVoti;
+
+    public void Stampa()
+    {
+        Console.WriteLine($"Dati studente: \nNome: {Nome}, \nMatricola: {Matricola}, \nMedia {MediaVoti}.");
+    }
+
+    public class Programma
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 2; i++)
+            {
+                Studente mioStudente = new Studente();
+                Console.WriteLine($"Inserire Nome Studente {i}:");
+                mioStudente.Nome = Console.ReadLine();
+                Console.WriteLine($"Inserire Matricola {i}:");
+                mioStudente.Matricola = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Inserire Media {i}:");
+                mioStudente.MediaVoti = double.Parse(Console.ReadLine());
+                mioStudente.Stampa();
+            }
+            
+
+        }
+    }
+}
